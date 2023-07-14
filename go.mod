73ab98e0ad6e63a4dcf1ef1631a1d18d17be86f5
vietdv277 <vietdv277@gmail.com>
2023-07-14 09:30:59 +0700

    Add bigbark funcs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/vietdv277/puppy
 
 go 1.20
+
+require github.com/vietdv277/dog v0.0.0-20230714022406-836a626b0467 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/vietdv277/dog"
+
 func Bark() string {
 	return "Wolf!"
 }
 
 func Barks() string {
 	return "Wolf.....Wolf.....Wolf"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
